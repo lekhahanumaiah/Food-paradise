1ec093057cffd9465df8b1e7bd9bfa7e5c165e52
lekhahanumaiah <145817415+lekhahanumaiah@users.noreply.github.com>
2025-07-20 18:58:18 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://food-paradise-backend.onrender.com",
         { firstName, lastName, email, phone, date, time },
         {
           headers: { "Content-Type": "application/json" },
